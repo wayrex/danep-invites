1569561503aa92adb92383330741b4d99538a6d3
Wayra Loayza <wayra91@gmail.com>
2024-01-17 20:25:09 -0400

    Add invitees option, format html

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,2 +1,9 @@
 const currentUrl = window.location.href;
+const twoInvitees = currentUrl?.endsWith('2');
+let inviteeAmount = 1;
+if (twoInvitees) {
+    inviteeAmount = 2;
+}
+document.querySelector("#inviteesAmount").innerText=inviteeAmount;
+
 console.log(currentUrl);
